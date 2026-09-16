ee65ada8cfc699d803e13274f7f058e88a7ad181
yamini102wani-sketch <yamini102wani@gmail.com>
2026-09-16 17:20:14 +0530

    Update admin.py

--- bloom_shop/admin.py
+++ bloom_shop/admin.py
@@ -1,7 +1,3 @@
-import os, django
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'crochet_site.settings')
-django.setup()
-from django.contrib.auth import get_user_model
-User = get_user_model()
-if not User.objects.filter(username='admin').exists():
-    User.objects.create_superuser('admin', 'admin@gmail.com', 'admin123')
+from django.contrib import admin
+from store.models import Product
+admin.site.register(Product)
